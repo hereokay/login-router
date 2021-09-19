7bb27120f3e2877aa194b4f8071d3ecadc99edd8
hereokay <hereokay88@gmail.com>
2021-09-19 13:28:23 +0900

    인증 필요없는 라우터 구현

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import NotFound from "./NotFound";
 
 function App() {
-  const [comp, setComp] = useState(Home);
-
   return (
-    <>
+    <Router>
       <header>
-        <button onClick={() => setComp(Home)}>Home</button>
-        <button onClick={() => setComp(About)}>About</button>
-        <button onClick={() => setComp(NotFound)}>Users</button>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/about">
+          <button>About</button>
+        </Link>
+        <Link to="/users">
+          <button>Users</button>
+        </Link>
       </header>
       <hr />
-      <main children={comp} />
-    </>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} /> 
+          <Route component={NotFound} />  {/* default page*/}
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
